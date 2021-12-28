872b8829bc5f0fc4b5ef6dbcd404c0463909b1e7
DeDomin98 <75976221+DeDomin98@users.noreply.github.com>
2021-12-28 13:07:11 +0100

    Update views.py

--- MySneakers/main/views.py
+++ MySneakers/main/views.py
@@ -42,4 +42,23 @@ def start(request):
     'total_value': total_value
     }
     
+    return render (request, 'main/start.html', context)
+
+def base(request):
+    sneakers = Sneakers.objects.all()
+
+    
+    total_value = 0
+    try:
+        for item in sneakers:
+            total_value += item.price
+    except:
+        pass
+
+    context = {
+    'sneakers': sneakers, 
+    'sneakers_count': len(sneakers),
+    'total_value': total_value
+    }
+    
     return render (request, 'main/base.html', context)
