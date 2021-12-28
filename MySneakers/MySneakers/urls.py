317d412c16c8c0c7b6ffc5a0c0ea251e36af0b68
DeDomin98 <75976221+DeDomin98@users.noreply.github.com>
2021-12-28 13:13:55 +0100

    Update urls.py

--- MySneakers/MySneakers/urls.py
+++ MySneakers/MySneakers/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('home', include('main.urls')),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
